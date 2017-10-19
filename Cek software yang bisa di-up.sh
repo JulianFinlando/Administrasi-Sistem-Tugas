90a2551b12e8ea2f290e769baa514b5f573b7375
SOMEONE-LAPTOP\eL <gamalael.wahyu@ti.ukdw.ac.id>
2017-10-19 12:24:13 +0700

    Cek program yang bisa di-up Ver1.0
    Versi Full code

--- Cek software yang bisa di-up.sh
+++ Cek software yang bisa di-up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+Update dan Upgrade berdasarkan penjadwalan lalu ditampilkan apa yang sudah diupdate
 
 echo "Selamat datang di program Upgrade Otomatis"
 
@@ -16,9 +17,48 @@ info=`apt list --upgradable`
 #Menampilkan daftar software yang upgradable
 
 for x in $info;
-	do echo $x;
+	
+do echo $x;
 
 done
 
 
+
+echo -n "Apakah Anda ingin menginstall semua update tersebut (y/n)?"
+
+read answer2
+
+if echo "$answer2" | grep -iq "^y" ;
+then
+
+if [ $(id -u) = 0 ];
+then #pengecekan user 
+root
+apt-get install unattended-upgrades
+dpkg-reconfigure unattended-upgrades
+
+if apt-get upgrade -y; 
+then
+    
+echo "$(date): Update Anda berhasil" >> hasil.txt
+	
+
+cat /var/log/apt/history.log >> hasil.txt
+else
+    
+echo "$(date): Update Anda gagal" >> hasil.txt
+	
+cat /var/log/apt/history.log >> hasil.txt
 fi
+
+elif [ $(id -u) != 0 ];
+then
+
+echo "Anda harus masuk ke user root"
+
+fi
+
+fi
+
+fi
+
